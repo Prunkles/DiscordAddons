fd59e4d8e506814647c3a8f3a71b0f82a9f6324d
Prunkles <prunkles.dreemurr@gmail.com>
2019-09-01 00:01:03 +0300

    Some docs

--- Discord.Net.BotMvc/CommandView/IMessageResult.cs
+++ Discord.Net.BotMvc/CommandView/IMessageResult.cs
@@ -5,6 +5,10 @@ namespace Discord.Net.BotMvc.CommandView
 {
     public interface IMessageResult : IResult
     {
+        /// <summary>
+        /// This method is used to send the result of returned in the command result as message on the channel.
+        /// </summary>
+        /// <param name="context">Context equal to the module context.</param>
         Task Send(ICommandContext context);
     }
 }
